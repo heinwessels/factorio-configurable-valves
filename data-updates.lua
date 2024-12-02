bd26a3530751c683d62615b430472f862b3e54f5
Branan Riley <branan@top-secret.network>
2024-12-02 12:03:28 -0800

    Add better support for replacing the pyindustry valves
    This detects when pyindustry is installed, and if it is it will
    use the tech unlock and recipe from the pyindustry valves rather
    than the pump

--- data-updates.lua
+++ data-updates.lua
@@ -1,10 +1,30 @@
+--[[
+If PyIndustry is installed, we want to be equivalent to its
+overflow/underflow valves. Otherwise we are equivalent to a pump.
+
+Since the fancy Py valves are currently disabled, we pick the tech
+based on the check valve, but still pull the recipe from the overflow
+valve for balance reasons.
+]]--
+
+---@type string
+local base_recipe_item
+local base_tech_item
+if mods.pyindustry then
+    base_recipe_item = "py-overflow-valve"
+    base_tech_item = "py-check-valve"
+else
+    base_recipe_item = "pump"
+    base_tech_item = "pump"
+end
+
 ---@type data.TechnologyPrototype?
 local tech_to_unlock
 for _, technology in pairs(data.raw.technology) do
     if technology.effects then			
         for index, effect in pairs(technology.effects) do
             if effect.type == "unlock-recipe" then
-                if effect.recipe == "pump" then
+                if effect.recipe == base_tech_item then
                   tech_to_unlock = technology
                   table.insert(tech_to_unlock.effects, index, {
                     type = "unlock-recipe",
@@ -23,7 +43,7 @@ local ingredients
 for _, recipe in pairs(data.raw.recipe) do
   for _, result in pairs(recipe.results or { }) do
       if result.type == "item" then
-          if result.name == "pump" then
+          if result.name == base_recipe_item then
             ingredients = recipe.ingredients
             break
           end
